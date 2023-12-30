f87d0fc027ca8371dfde0a5bf669606bc03809cd
muhammad bilal yameen <bilalyameen03151190023@gmai.com>
2023-12-29 17:15:33 -0800

    class 2

---
+++ main.js
@@ -0,0 +1,62 @@
+// practise 1
+var powder = "Lal mirch powder";
+// powder = "haldi"
+console.log("hello");
+console.log(powder);
+// practise 2
+// let fullName:string = "bilal";
+// // fullName = "ali";
+// // fullName = "syed";
+// console.log(fullName);
+// practise 3
+// let  fullName = "Muhammad Bilal"
+// let age = 23
+// let bulb = true
+// let buld = false
+// console.log(fullName)
+// practise 4
+// let firstName = "Bilal"
+// let lastName = "Yamenn"
+// // console.log(firstName + " " + lastName)
+// console.log(`my first is ${firstName } `)
+// console.log(`my last is ${lastName } `)
+// practise 5
+// let qty = 22
+// console.log(qty+ 10)
+// console.log (qty - 10)
+// console.log (qty * 10)
+// console.log (qty / 3)
+// practise 6
+// let str1 = "100"
+// let str2 = "22"
+// console.log(str1 + str2)
+// practise 7
+// let n1 = 2
+// let n2 = 3
+// console.log(n1**n2)
+// practise 8
+// let age = 22
+// // age += 10
+// // age  -= 10
+// console.log(age)
+// practise 9
+// let n = 5
+// n -= 5
+// console.log(n)
+// practise 10
+// let age:number = 23
+// console.log( age ==23)
+// console.log(age == 12 )
+// console.log(age != 23)
+// console.log(age > 22)
+// console.log(age >= 23)
+// console.log(age >= 23)
+//practise 11
+// let n = 5
+// console.log(n == 5)      true
+// console.log(n === 5)     true
+// console.log(n != 5)      false
+// console.log(n > 5)       false
+// console.log(n < 5)       false
+// console.log(n <= 8)      false
+// console.log(n <=8)       true 
